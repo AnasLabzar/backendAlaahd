faf15d69af344334696aed95558eff77d7ae5779
AnasLabzar <principeanas80@gmail.com>
2024-10-11 22:54:41 +0100

    update db skus

--- src/models/Sku.ts
+++ src/models/Sku.ts
@@ -1,14 +1,14 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 interface ISKU extends Document {
-  skuCode: string;
-  productId: string;
+  sku: string;
+  barcode: string;
 }
 
 const SKUSchema: Schema = new Schema({
   skuCode: { type: String, required: true },               // SKU code for product
-  productId: { type: String, required: true },             // Reference to product ID
+  barcode: { type: String },             // Reference to product ID
   fetchedAt: { type: Date, default: Date.now },            // Record fetch date
 });
 
-export const SKU = mongoose.model<ISKU>('SKU', SKUSchema);
+export const SKU = mongoose.model<ISKU>('skus', SKUSchema);
