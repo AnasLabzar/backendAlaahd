4f37c9c121dd0890f5835c2720f4bddae2018c32
AnasLabzar <77747369+AnasLabzar@users.noreply.github.com>
2024-11-04 17:52:05 +0100

    Update authentication.ts

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -2,21 +2,21 @@ import express from 'express';
 
 import { loginUser, registerUser } from '../controllers/authController';
 import { isAuthenticated } from '../middlewares/roleMiddleware';
-import { getUsersBySessionToken } from '../controllers/userController';
+import { getUserBySessionToken } from '../controllers/userController';
 
 export default (router: express.Router) => {
     router.post('/auth/register', registerUser)
     router.post('/auth/login', loginUser)
+    
     // Add route to get user by session token
     router.get('/users/session/:sessionToken', isAuthenticated, async (req, res) => {
         try {
             const { sessionToken } = req.params;
-            const user = await getUsersBySessionToken(sessionToken);
+            const user = await getUserBySessionToken(sessionToken);
             return res.status(200).json(user);
         } catch (error) {
             console.error('Error fetching user by session token:', error);
             return res.status(404).json({ error: 'User not found' });
         }
     });
-
 };
