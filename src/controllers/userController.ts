ab8cd3a80fca0eb47c7a20d9394f21451b14637a
AnasLabzar <77747369+AnasLabzar@users.noreply.github.com>
2024-11-11 01:18:49 +0100

    Update userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { UserModel } from '../models/User';
+import { User } from '../models/User';
 
 export const getAllUsers = async (req: Request, res: Response) => {
     try {
-        const users = await UserModel.find();
+        const users = await User.find();
         return res.status(200).json(users);
     } catch (error) {
         console.log(error);
@@ -11,63 +11,63 @@ export const getAllUsers = async (req: Request, res: Response) => {
     }
 }
 
-// Function to get user by ID
-export const getUsersById = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params; // Extract id from request parameters
-        const user = await UserModel.getUserById(id);
-        console.log(user);
-        return res.status(200).json(user);
-    } catch (error) {
-        console.log(error);
-        return res.status(404).json({ error: 'User not found' });
-    }
-}
+// // Function to get user by ID
+// export const getUsersById = async (req: Request, res: Response) => {
+//     try {
+//         const { id } = req.params; // Extract id from request parameters
+//         const user = await UserModel.getUserById(id);
+//         console.log(user);
+//         return res.status(200).json(user);
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(404).json({ error: 'User not found' });
+//     }
+// }
 
-// Function to get user by session token
-export const getUsersBySessionToken = async (sessionToken: string) => {
-    try {
-        const user = await UserModel.getUserBySessionToken(sessionToken); // Corrected the function call here
-        return user;
-    } catch (error) {
-        throw new Error('User not found');
-    }
-}
+// // Function to get user by session token
+// export const getUsersBySessionToken = async (sessionToken: string) => {
+//     try {
+//         const user = await UserModel.getUserBySessionToken(sessionToken); // Corrected the function call here
+//         return user;
+//     } catch (error) {
+//         throw new Error('User not found');
+//     }
+// }
 
 
 
-export const deleteUser = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const deletedUser = await UserModel.deleteUserById(id);
-        return res.json(deletedUser);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(400)
-    }
-}
+// export const deleteUser = async (req: Request, res: Response) => {
+//     try {
+//         const { id } = req.params;
+//         const deletedUser = await UserModel.deleteUserById(id);
+//         return res.json(deletedUser);
+//     } catch (error) {
+//         console.log(error);
+//         res.sendStatus(400)
+//     }
+// }
 
-export const updateUser = async (req: Request, res: Response) => {
-    try {
-        const { id } =  req.params;
-        const { username } = req.body;
+// export const updateUser = async (req: Request, res: Response) => {
+//     try {
+//         const { id } =  req.params;
+//         const { username } = req.body;
 
-        if (!username) {
-            return res.sendStatus(400)
-        }
+//         if (!username) {
+//             return res.sendStatus(400)
+//         }
 
-        // Retrieve user by ID
-        const user = await UserModel.getUserById(id);
+//         // Retrieve user by ID
+//         const user = await UserModel.getUserById(id);
 
-        // Update user's username
-        user.username = username;
+//         // Update user's username
+//         user.username = username;
 
-        // Save the updated user
-        await user.save();
+//         // Save the updated user
+//         await user.save();
 
-        return res.status(200).json(user).end();
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(400);
-    }
-}
+//         return res.status(200).json(user).end();
+//     } catch (error) {
+//         console.log(error);
+//         res.sendStatus(400);
+//     }
+// }
