941924f8da1218b838b4d14eed4e097876768c63
AnasLabzar <77747369+AnasLabzar@users.noreply.github.com>
2024-11-13 20:47:35 +0100

    Update colorRoutes.ts

--- src/routes/colorRoutes.ts
+++ src/routes/colorRoutes.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
-import { createColor, getColors, getColorsById } from '../controllers/colorController';
+import { 
+  createColor, 
+  getColors, 
+  getColorsById, 
+  updateColor, 
+  deleteColor 
+} from '../controllers/colorController';
 
 const router = Router();
 
 router.post('/colors', createColor);
 router.get('/colors', getColors);
 router.get('/colors/:id', getColorsById);
+
+// Update a color by ID
+router.put('/colors/:id', updateColor);
+
+// Delete a color by ID
+router.delete('/colors/:id', deleteColor);
 
 export default router;
