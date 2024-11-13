2667c32841671913a83526eb8f5814a4dc949c45
AnasLabzar <77747369+AnasLabzar@users.noreply.github.com>
2024-11-13 20:51:42 +0100

    Update productRoutes.ts

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
-import { createProduct, getProductById, getAllProducts } from '../controllers/productController';
+import { 
+  createProduct, 
+  getProductById, 
+  getAllProducts, 
+  updateProduct, 
+  deleteProduct 
+} from '../controllers/productController';
 
 const router = Router();
 
 router.post('/products', createProduct);
 router.get('/products/:id', getProductById);
 router.get('/products', getAllProducts);
+
+// Update a product by ID
+router.put('/products/:id', updateProduct);
+
+// Delete a product by ID
+router.delete('/products/:id', deleteProduct);
 
 export default router;
